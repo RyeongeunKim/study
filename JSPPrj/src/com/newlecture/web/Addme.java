0b74c843b35b994534b0cfd684c465711b454a50
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-06-03 22:39:08 +0900

    서블릿 뉴렉처

--- JSPPrj/src/com/newlecture/web/Addme.java
+++ JSPPrj/src/com/newlecture/web/Addme.java
@@ -1,32 +1,41 @@
 package com.newlecture.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/add")
-public class Add extends HttpServlet {
-
+@WebServlet("")
+public class Addme extends HttpServlet {
+	
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html; charset=UTF-8");
 		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+				
 		String x_ = request.getParameter("x");
 		String y_ = request.getParameter("y");
-		
+				
 		int x = 0;
 		int y = 0;
 		
-		if(!x_.equals("")) x = Integer.parseInt(x_);
-		if(!y_.equals("")) y = Integer.parseInt(y_);
+		if(x_ != null && !x_.equals(""))
+			x = Integer.parseInt(x_);
 		
-		int result = x+y;
+		if(y_ != null && !y_.equals(""))
+			y = Integer.parseInt(y_);
 		
-		response.getWriter().printf("result is %d\n", result);
+		out.print("덧셈 결과 : " + (x+y));
+		
+		
+		
 	}
-
+	
 }
