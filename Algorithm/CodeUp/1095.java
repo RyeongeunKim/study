d6da488e1b0e7ed43fad900e1b574056ddfd43ca
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-07-18 21:11:18 +0900

    21.07.18(일) 이상한 출석 번호 부르기3(1차원배열)
    ;
    ;

---
+++ Algorithm/CodeUp/1095.java
@@ -0,0 +1,20 @@
+﻿import java.util.*;
+
+class Main {  
+  public static void main(String args[]) { 
+    Scanner sc = new Scanner(System.in);
+
+    int min=0;
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++){
+      arr[i] = sc.nextInt();
+      if(i == 0){
+        min = arr[i];
+      } else if(min>arr[i]){
+        min = arr[i];
+      }
+    }
+    System.out.print(min);
+  } 
+}
