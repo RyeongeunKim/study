b8ed83d07be04f36416d856a8e477279f0667b55
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-07-20 21:29:34 +0900

    21.07.20(화) [2차원배열]바둑알 십자 뒤집기

---
+++ Algorithm/CodeUp/1097.java
@@ -0,0 +1,38 @@
+﻿import java.util.*;
+
+class Main {  
+  public static void main(String args[]) { 
+    Scanner sc = new Scanner(System.in);
+	  int[][] arr = new int[19][19];
+
+	  for(int i=0; i<arr.length; i++){
+		  for(int j=0; j<arr.length; j++){
+			  arr[i][j] = sc.nextInt();
+		  }
+	  }
+
+	  int n = sc.nextInt();
+
+	  for(int i=0; i<n; i++){
+		  int x = sc.nextInt()-1;
+		  int y = sc.nextInt()-1;
+
+		  for(int j=0; j<arr.length; j++){
+			  if(arr[x][j] == 0) arr[x][j] = 1;
+			  else arr[x][j] = 0;
+		  }
+
+		  for(int j=0; j<arr.length; j++){
+			  if(arr[j][y] == 0) arr[j][y] = 1;
+			  else arr[j][y] = 0;
+		  }
+	  }
+
+	  for(int i=0; i<arr.length; i++){
+		  for(int j=0; j<arr.length; j++){
+			  System.out.print(arr[i][j] + " ");
+		  }
+		  System.out.println();
+	  }
+  } 
+}
