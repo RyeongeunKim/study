bdcf6556886b4124356024e66339ee3a8ba8e40f
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-08-01 20:53:07 +0900

    21.08.01(일) BufferedReader와 BufferedWriter 사용하여  빠른 A+B계산

---
+++ Algorithm/BaekJoon/for_빠른A+B.java
@@ -0,0 +1,31 @@
+﻿import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {  
+  public static void main(String args[]) throws IOException { 
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	  
+	  int t = Integer.parseInt(br.readLine());
+
+	  // 입력받을 때 한줄로 받기 때문에 사용
+	  // split처럼 사용함
+	  StringTokenizer st;
+	  
+	  for(int i=0; i<t; i++){
+		  st = new StringTokenizer(br.readLine(), " ");
+		  int a = Integer.parseInt(st.nextToken());
+		  int b = Integer.parseInt(st.nextToken());
+
+		  //개행 문자열 사용 시 자동으로 String으로 바뀐다.
+		  bw.write(a+b+"\n");
+	  }
+
+	  // flush 사용하여 한꺼번에 쏟아내기
+	  bw.flush();
+	  
+	  bw.close();
+	  br.close();
+	  
+  } 
+}
---
+++ Algorithm/BaekJoon/for_빠른A+B_숏코딩.java
@@ -0,0 +1,19 @@
+﻿import java.io.*;
+
+class Main {  
+  public static void main(String args[]) throws IOException { 
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	  int t = Integer.parseInt(br.readLine());
+
+	  for(int i=0; i<t; i++){
+		  String[] arr = br.readLine().split(" ");
+		  int a = Integer.parseInt(arr[0]);
+		  int b = Integer.parseInt(arr[1]);
+		  bw.write(a+b + "\n");
+	  }
+	  bw.flush();
+	  bw.close();
+  } 
+}
