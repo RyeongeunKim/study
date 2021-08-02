7933a7856ac793a8b2aa7d184fb4eae9891457ae
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-08-02 10:16:07 +0900

    21.08.02(월) for문 연습

---
+++ Algorithm/BaekJoon/A+B출력.java
@@ -0,0 +1,20 @@
+﻿import java.io.*;
+
+class Main {  
+  public static void main(String args[]) throws IOException { 
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	  int n = Integer.parseInt(br.readLine());
+
+	  for(int i=1; i<=n; i++){
+		  String[] arr = br.readLine().split(" ");
+		  int a = Integer.parseInt(arr[0]);
+		  int b = Integer.parseInt(arr[1]);
+		  bw.write("Case #"+i+": "+(a+b)+"\n");
+	  }
+	  bw.flush();
+	  bw.close();
+	  br.close();
+  } 
+}
