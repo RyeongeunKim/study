0263c26eaa361287bdfac58096ed2810afc7a6a4
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-08-05 22:12:21 +0900

    21.08.05 while_a+b_BufferedReader

---
+++ Algorithm/BaekJoon/while_A+B출력_BufferedReader.java
@@ -0,0 +1,23 @@
+﻿import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {  
+  public static void main(String args[]) throws IOException { 
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	  StringTokenizer st;
+	  StringBuilder sb = new StringBuilder();
+
+	  while(true){
+		  st = new StringTokenizer(br.readLine()," ");
+		  int a = Integer.parseInt(st.nextToken());
+		  int b = Integer.parseInt(st.nextToken());
+
+		  if(a==0 && b==0){
+			  break;
+		  }
+		  sb.append((a+b)).append('\n');
+	  }
+	  System.out.println(sb);
+  } 
+}
