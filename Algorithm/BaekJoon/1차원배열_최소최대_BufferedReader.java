681cf34e500a2229e36dc9baa001a94c42865156
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-08-10 15:30:34 +0900

    21.08.10(화) 1차원배열_최소,최대

---
+++ Algorithm/BaekJoon/1차원배열_최소최대_BufferedReader.java
@@ -0,0 +1,26 @@
+﻿import java.io.*;
+import java.util.*;
+
+class Main {  
+  public static void main(String args[]) throws IOException { 
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	  int n = Integer.parseInt(br.readLine());
+	  StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	  int max = -1000000;
+	  int min = 1000000;
+	  StringBuilder sb = new StringBuilder();
+
+	  for(int i=0; i<n; i++){
+		  int a = Integer.parseInt(st.nextToken());
+		  if(a>max){
+			  max = a;
+		  }
+		  if(a<min){
+			  min = a;
+		  }
+	  }
+	  sb.append(min).append(" ").append(max);
+	  System.out.println(sb);
+  } 
+}
---
+++ Algorithm/BaekJoon/1차원배열_최소최대_Scanner.java
@@ -0,0 +1,30 @@
+﻿import java.util.*;
+
+class Main {  
+  public static void main(String args[]) { 
+	  Scanner sc = new Scanner(System.in);
+	  int n = sc.nextInt();
+	  int[] data = new int[n];
+
+	  for(int i=0; i<n; i++){
+		  data[i] = sc.nextInt();
+	  }
+
+	  int min = data[0]; //가장 큰수
+	  int max = data[0];
+
+	  for(int i=0; i<n; i++){
+		  if(min > data[i]){
+			  min = data[i];
+		  }
+	  } //최소값
+
+	  for(int i=0; i<n; i++){
+		  if(max < data[i]){
+			  max = data[i];
+		  }
+	  }
+
+	  System.out.println(min +" "+ max);
+  } 
+}
