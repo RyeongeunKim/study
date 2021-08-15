da2c0d6ab247b2a7db29aee686f494c21dafd1d4
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-08-15 17:36:12 +0900

    21.08.15 [1차원배열]최댓값_BufferedReader

---
+++ Algorithm/BaekJoon/1차원배열_최댓값_BufferedReader.java
@@ -0,0 +1,25 @@
+﻿import java.io.*;
+
+class Main {  
+  public static void main(String args[]) throws IOException { 
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  int[] num = new int[9];
+	  int count = 0;
+	  int max = 0;
+	  for(int i=0; i<num.length; i++){
+		  num[i] = Integer.valueOf(br.readLine());
+		  if(num[i] > 99)
+			  System.exit(0);
+	  }
+
+	  for(int i=0; i<num.length; i++){
+		  if(num[i] > max){
+			  max = num[i];
+			  count = i+1;
+		  }
+	  }
+
+	  System.out.println(max);
+	  System.out.println(count);
+  } 
+}
