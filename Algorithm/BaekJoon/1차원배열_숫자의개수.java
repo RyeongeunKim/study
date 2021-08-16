c33ef8cf67680e1aaebe3ed5f4d7f0822e87eaf4
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-08-16 13:51:20 +0900

    21.08.16(월) [1차원배열]숫자의개수

---
+++ Algorithm/BaekJoon/1차원배열_숫자의개수.java
@@ -0,0 +1,29 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {  
+  public static void main(String args[]) throws IOException{ 
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	  int a = Integer.parseInt(br.readLine());
+	  int b = Integer.parseInt(br.readLine());
+	  int c = Integer.parseInt(br.readLine());
+
+	  int n = a * b * c;
+
+	  int[] count = new int[10];
+	  for(int i=0; i<10; i++) {count[i] = 0;}
+
+	  while(n!=0){
+		  count[n%10]++;
+		  n /= 10;
+	  }
+
+	  for(int num : count){
+		  System.out.println(num);
+	  }
+
+	  br.close();
+  } 
+}
