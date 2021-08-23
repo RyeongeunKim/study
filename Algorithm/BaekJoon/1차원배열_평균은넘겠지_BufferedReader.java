f39b6c0bc4473dfdc367463b84ad11ae07217e90
Ryeongeun Kim <ryeongeun.gim@gmail.com>
2021-08-23 20:53:17 +0900

    21.08.23(월) 평균은넘겠지

---
+++ Algorithm/BaekJoon/1차원배열_평균은넘겠지_BufferedReader.java
@@ -0,0 +1,40 @@
+﻿import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main {  
+  public static void main(String args[]){ 
+	  try{
+		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		  int count = Integer.parseInt(br.readLine());
+
+		  // 외부 for문
+		  for(int i=0; i<count; i++){
+			  StringTokenizer st = new StringTokenizer(br.readLine());
+			  int innerCount = Integer.parseInt(st.nextToken());
+			  int[] group = new int[innerCount];
+			  int sum = 0;
+			  // 내부 for문 
+			  for(int j=0; j<innerCount; j++){
+				  group[j] = Integer.parseInt(st.nextToken());
+				  sum += group[j];
+			  }
+			  int avg = sum/innerCount;
+			  int check = 0;
+			  for(int k : group){
+				  if(Integer.compare(k, avg) > 0){
+					  check += 1;
+				  }
+			  } // 내부 for문 end
+			  bw.write(String.format("%.3f",check*100.000/innerCount)+"%\n");
+		  } // 외부 for문 end
+		  bw.flush();
+		  bw.close();
+	  } catch (Exception e){
+		  e.printStackTrace();
+	  }
+  } 
+}
---
+++ Algorithm/BaekJoon/1차원배열_평균은넘겠지_Scanner.java
@@ -0,0 +1,32 @@
+﻿import java.util.Scanner;
+
+class Main {  
+  public static void main(String args[]) { 
+	  Scanner sc = new Scanner(System.in);
+	  int testCase = sc.nextInt(); // 테스트 케이스 수 입력
+
+	  for(int i=0; i<testCase; i++){
+		  int stuNum = sc.nextInt(); // 학생 수
+		  int count = 0; // 평균을 넘는 학생 수
+		  double avg = 0; // 평균 점수
+		  double avgOver; // 평균을 넘는 학생 비율
+		  int[] scoreArr = new int[stuNum]; // 각 점수를 저장할 배열 생성
+		  for(int j=0; j<stuNum; j++){
+			  int score = sc.nextInt();
+			  scoreArr[j] = score; // 각 점수를 배열에 저장
+			  avg += score; // 점수의 합 구하기
+		  }
+		  avg = avg / stuNum; // 평균 점수 구하기
+		  for(int k=0; k<scoreArr.length; k++){
+			  if(scoreArr[k]>avg){
+				  count++;
+			  }
+		  }
+		  avgOver = (double) count / stuNum * 100; // count를 double로 강제형변환시켜서 원하는 백분율 계산하기
+		  											// stuNum은 double / int이므로 자동형변환이 일어남
+		  System.out.printf("%.3f", avgOver);
+		  System.out.println("%");
+	  }
+	  
+  } 
+}
