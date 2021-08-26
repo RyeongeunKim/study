d8879c83dcbb772d5e0f77054e74d8894b828b53
ryeongeunkim <ryeongeun.gim@gmail.com>
2021-08-26 17:12:07 +0900

    21.08.26 [MODIFY] 제어문, 객체지향 프로그래밍

--- Douzone/Hello.java
+++ Douzone/Hello.java
@@ -11,7 +11,361 @@ public class Hello
 }
 */
 
+class Tiger{
+	
+	void merry() {
+		System.out.println('멍');
+	}
+	// int num = 3
+	void happy(int num) {
+		for(int i=0; i<num; i++) {
+			System.out.println('왕');			
+		}
+	}
+	
+	int turtle() {
+		System.out.println("거북이 콜");
+		return 15;
+	}
+	
+	int fish(int num) {
+		int n = num * 3;
+		return n;
+	}
+}
 
+// ex18)
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		// 객체 생성
+		Tiger t = new Tiger();
+		t.merry();
+		t.merry();
+		t.merry();		
+
+		
+		for(int i=0; i<10; i++) {
+			t.merry();
+		}
+		
+		System.out.println("---------------------------------");
+		t.happy(5);
+		
+		System.out.println("---------------------------------");
+		int num = t.turtle();
+		System.out.println(num);
+		System.out.println(t.turtle());
+		
+		System.out.println("---------------------------------");
+		int cp = t.fish(7);
+		System.out.println(cp);
+		System.out.println(t.fish(345));
+		
+		// 리턴값   인수전달
+		// x       x
+		// x       o
+		// o       x
+		// o       o
+	}
+}
+
+/*
+class Tiger{
+	// 필드 : 클래스 안에서 선언되는 변수
+	int a;
+	int b = 20;
+	int c, d = 30;
+	
+	// 생성자 
+	// 메소드
+}
+
+// ex17) 
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		Tiger t1 = new Tiger(); // 객체생성
+		Tiger t2 = new Tiger(); // 객체생성
+		t1.a = 100;
+		t1.c = t1.a + 30;
+		System.out.println(t1.a);
+		System.out.println(t1.b);
+		System.out.println(t1.c);
+		System.out.println(t1.d);
+		System.out.println(t2.c);
+		
+		// 모든 객체는 생성이 될 때 반드시 고유한 번호를 발급받는다
+		System.out.println(t1.hashCode());
+		System.out.println(t2.hashCode());
+		
+		Tiger t3;
+		t3 = t2;
+		
+		// t2와 t3는 메모리를 공유한다
+		// 동일한 메모리를 같은 이름으로 쓰고있다
+		System.out.println(t3.hashCode()); 
+		
+		
+		t3.a = 999;
+		System.out.println(t2.a);
+	}
+}
+*/
+/*
+// ex15) 대입, 증가 연산자
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		// 대입 -> 증가 순서
+		int a = 10, b;
+		b = a++;
+		System.out.println(a + " " + b);
+		
+		// 증가 -> 대입 순서
+		int c = 10;
+		int d = 0;
+		d = ++c;
+		System.out.println(c + " " + d);
+		
+		int a1 = 10, a2 = 10;
+		System.out.println(a1++);
+		System.out.println(++a2);
+		System.out.println(a1 + " " + a2);
+	}
+}
+*/
+/*
+// ex14) 우박수 출력 프로그램
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		// 예제 1
+		int n = 77;
+		while(true) {
+			System.out.println(n);
+//			if(n%2==0) {
+//				n = n / 2;
+//			} else {
+//				n = n * 3 + 1;
+//			}
+			n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
+			
+			if(n==1) {
+				System.out.println(n);
+				System.out.println("탈출하기 직전");
+				break;
+			}
+		}
+		
+		// 예제 2
+		int n2 = 1234;
+		int sum = 0;
+		while(true) {
+			int r = n2 % 10;
+			n2 = n2 / 10;
+			System.out.println(n2 + " " + r);
+			sum = sum + r;
+			if(n2 == 0) {
+				break;
+			}
+		}
+		System.out.println(sum);
+		
+		// int : 타입
+		// a : 변수
+		// = : 대입연산자
+		// 10 : 리터럴
+		int a = 10;
+		int b = 20;
+		short c = 30;
+		
+		// 대입연산자는 쌍방간의 타입이 같아야한다
+		// 받는 타입으로 똑같이 바꿔주기
+		// b 4byte, c 2byte
+		b = (int)c;
+		// c 2byte, b 4byte
+		c = (short)a;
+		
+		int num = 10;
+		num = num + 3;
+		num += 3; // 위 코드보다 더 많이 사용함
+		int a1 = 0;
+		int a2 = 0;
+		a1 = a2+3;
+		int t = +1;
+		int u = -1;
+		
+		int k = 10;
+		k += 3;
+		System.out.println(k);
+		k = +3;
+		System.out.println(k);
+		
+		int d = 0;
+		d++;
+		System.out.println(d);
+		++d;
+		System.out.println(d);
+		d = d+1;
+		System.out.println(d);
+		d += 1;
+		System.out.println(d);
+	}
+}
+*/
+// ex13) 제어문 -  while
+// 처음부터 반복횟수를 알 때는 for문, 모를 때는 while문
+/*
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+//		int num = 0;
+//		while(true) {
+//			System.out.println(num + " ");
+//			num++;
+//		}
+		
+		int a = 0;
+		while(true) {
+			System.out.println(a);
+			if(a==5) {
+				System.out.println("탈출하기 직전");
+				break;
+			}
+			a++;
+		}
+		
+	}
+}
+*/
+/*
+// ex12) 제어문 - for문 
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum = sum + i;
+			System.out.println(sum);
+		}
+		
+		int n = 7;
+		for (int i = 0; i < 10; i++) {
+			System.out.println(n+" * "+i+" = "+n*i);
+		}
+	}
+}
+*/
+/*
+// ex11) 제어문 - for문
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		// 피해야되는 문법
+		// i는 0,1을 제외한 다른 숫자가 들어가면 안된다!
+		// 부등호 부호 바꾸면 안됨 (>)
+		// 증가횟수가 1증가말고 다른 숫자는 안된다!
+		//예)
+		 //for (int i = 5; i > 4; i=i+3) {
+		 //
+		 //}
+		 
+		
+		// 1형식
+		for (int i = 0; i < 5; i++) {
+			
+		}
+		
+		// 2형식
+		for (int i = 1; i <= 5; i++) {
+			
+		}
+		
+		// 3형식
+		// 음수, 양수의 좌표값 제어
+		// 반복횟수 5*2+1
+		int n = 5;
+		for (int i = -n; i <= n; i++) {
+			
+		}
+		
+		
+		// 기본
+		for (int i = 0; i < 3; i++) {
+			System.out.println("호랑이"+i);
+			int num = 10;
+			num = num + 3;
+			System.out.println(num);
+			num = num + 1;
+			System.out.println(num);
+			num++;
+			System.out.println(num);
+			num--;
+			System.out.println(num);
+			++num;
+			System.out.println(num);
+			--num;
+			System.out.println(num);
+		}
+	}
+}
+*/
+/*
+// ex10) 제어문 - switch문
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		// 1) case 뒤에는 변수가 올 수 없음
+		// 2) switch 안에는 변수가 올 수 있음		
+		// 3) default는 생략가능
+		// 4) 의도적인 경우는 break는 생략가능
+		// 5) case문장에서 실행되는 한 문장 이상일 때는 일반적으로
+		//    블록처리를 해버린다
+		
+		int num = 10;
+		
+		switch (10) {
+		case 10:
+			System.out.println(1);
+			//break;
+		case 20:{
+			System.out.println(2);
+			System.out.println(100);			
+		} break;
+		case 30:
+			System.out.println(3);
+			break;
+		default:
+			System.out.println(4);
+			break;
+		}
+		
+		int jumsu = 83;
+		int q = jumsu / 10;
+		if(q == 9) {
+			System.out.println("A학점입니다");
+		} else if(q == 8) {
+			System.out.println("B학점입니다");
+		} else if(q == 7) {
+			System.out.println("C학점입니다");
+		} else if(q >= 6) {
+			System.out.println("D학점입니다");
+		} else {
+			System.out.println("F학점입니다");
+		}
+	}
+}
+*/
+/*
 // ex9) 제어문 - if문
 public class Hello 
 {
@@ -62,9 +416,24 @@ public class Hello
 		
 		System.out.println(a > b ? "호랑이" : "코끼리");
 		
+		// 4) else-if => n개 중에 한개
+		//switch로 바꿀 수 있다면 바꾸는게 원칙!
+		int num = 83;
+		
+		if(num >= 90) {
+			System.out.println("A학점입니다");
+		} else if(num >=80) {
+			System.out.println("B학점입니다");
+		} else if(num >=70) {
+			System.out.println("C학점입니다");
+		} else if(num >=60) {
+			System.out.println("D학점입니다");
+		} else {
+			System.out.println("F학점입니다");
+		}
 	}
 }
-
+*/
 /*
 // ex8)
 public class Hello 
