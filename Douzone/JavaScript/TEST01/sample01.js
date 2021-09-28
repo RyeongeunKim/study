b49362fa4b4d755b2ab75005ce77f182497ee136
unknown <ryeongeun.gim@gmail.com>
2021-09-28 19:39:38 +0900

    21.09.28(화) [ADD] 비동기 함수, 객체, 배열

--- Douzone/JavaScript/TEST01/sample01.js
+++ Douzone/JavaScript/TEST01/sample01.js
@@ -1,4 +1,334 @@
 
+/*
+// ex40) 배열 안 객체
+
+let ar = [
+    {
+        n: '호랑이',
+        a: 10,
+    },
+    {
+        n: '코끼리',
+        a: 20,
+    },
+    {
+        n: '독수리',
+        a: 30,
+    },
+]
+console.log(ar);
+
+for (const key in ar) {
+    console.log(key);
+    let e = ar[key];
+    console.log(e);
+    //console.log(e.n, e.a);
+    console.log(e['n'], e['a']);
+}
+
+let sum = 0;
+for (const v of ar) {
+    sum += v.a;
+}
+console.log(sum);
+
+let br = ar.map(
+    (v, k)=> v.n + v.a
+)
+
+console.log(br);
+*/
+
+/*
+// ex39) forin | forof | foreach
+let ar = [10, 20, 30];
+console.log(ar);
+
+// forin = index 이용하여 출력하기
+for (const index in ar) {
+    console.log(index, ar[index]);
+}console.log('--------------');
+
+// forof - 값을 받아서 사용
+for (const value of ar) {
+    console.log(value);
+}console.log('--------------');
+
+// foreach - 배열에 내장되어 있는 iterator 사용
+ar.forEach(
+    function(v, i){
+        console.log(v, i);
+    }
+);console.log('1--------------');
+
+// 위 코드 람다로 변형
+ar.forEach((v, i)=>{
+        console.log(v, i);
+});console.log('2--------------');
+
+// map 함수 - 기존의 데이터를 가공해서 새로운 데이터를 만든다 
+let br = ar.map(
+    (num)=>{
+        return num * 10;
+    }
+)
+console.log(br);
+
+// 위와 같은 코드
+br = ar.map(num=> num * 10)
+console.log(br);
+
+let cr = [0, 1, 2, 3]
+let dr = cr.map(
+    // num => {
+    //     if(num % 2)
+    //         return 'odd'
+    //     else 
+    //         return 'even'
+    // }
+
+    // 삼항연산
+    // num => {
+    //     return (num % 2) ? 'odd' : 'even'
+    // }
+
+    num => (num % 2) ? 'odd' : 'even'
+);
+console.log('');
+*/
+
+/*
+// ex38) 배열3 - 배열 안에 데이터넣기
+let ar = [
+    10,
+    '호랑이',
+    true,
+    [10, 20, 30], // 배열 안에 배열
+    {name:'독수리', age:10}, // 객체
+    function(){},
+    undefined
+];
+console.log(ar);
+*/
+
+/*
+// ex37) 배열2 - 배열만들기
+let ar = [10, 20, 30];
+let br = Array(); // []
+let cr = Array(5);
+let dr = Array(10, 20, 30);
+console.log(dr);
+*/
+
+/*
+// ex36) 배열1
+let ar = [10, 20, 30];
+console.log(ar);
+console.log(ar.length); // 배열길이
+console.log(typeof(ar)); // 타입
+console.log(Array.isArray(ar)); // 배열이 맞으면 true
+
+*/
+
+/*
+// ex35) 객체6 - 객체 병합 문법
+let obj01 = {
+    a:10,
+    b:20,
+}
+let obj02 = {
+    c:30,
+    d:40,
+}
+
+// 병합하고 싶은 객체 이름 넣기
+let obj03 = Object.assign(obj01, obj02);
+console.log(obj03);
+
+let obj04 = {...obj01, ...obj02} 
+console.log(obj04);
+
+let f1 = (o) => {
+    console.log(o.d);
+}
+f1({...obj01, ...obj02})
+*/
+/*
+// ex34) 객체5 - 객체의 키, 값 추출
+let obj = {
+    a:10,
+    b:20,
+}
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+*/
+
+/*
+// ex33) 객체4
+let obj = {
+    a:10,
+}
+obj.b = 20;
+console.log(obj);
+
+obj['c'] = 30;
+console.log(obj);
+
+for (let i = 0; i < 4; i++) {
+    obj['tiger'+i] = i*100;
+}
+console.log(obj);
+*/ 
+
+/*
+// ex32) 객체3 : 외부의 변수가 가지고 있는 값을 키값으로 사용
+
+let c = 'tiger';
+let obj = {
+    a:10,
+    b:20,
+    [c]:30 // c가 가지고 있는 값을 키값으로 사용함
+}
+console.log(obj.a, obj.b, obj.tiger);
+*/
+
+/*
+// ex31) 객체2
+let obj = {
+    a:10,
+    b:20,
+    f1:function(){
+        console.log(
+            this.a, 
+            this.b);
+    },
+// f2:()=>{ 
+//     console.log(
+//         this.a, // 에러발생 -> 람다에서는 this를 사용할 수 없다
+//         this.b);
+// }, // <- 문법에러 아님, 끝났다는 의미? 가끔 사용
+    f2:function(){
+        for(const key in this){
+            console.log(this[key]);
+        }
+    }
+}
+obj.f1();
+console.log('------------------------');
+obj.f2();
+*/
+/*
+// ex30) 객체1
+let obj = {
+    a:10,
+    b:20,
+    f1:()=>{console.log('f1');
+    }
+}
+console.log(obj);
+obj.b = 99; // 데이터 갱신
+obj.c = 30; // 데이터 추가 - 없는 속성을 사용하면 만들어진다
+console.log(obj);
+
+delete(obj.b) // 속성 제거
+console.log(obj);
+
+obj.f2 = () => {console.log('test');}
+obj.f2();
+delete(obj.f1); // 함수 제거
+*/
+
+/*
+// ex29) 키값출력
+let t = {
+    name:'호랑이',
+    age:100,
+    bl:true
+}
+
+for (const key in t) {
+    //console.log(key); // 키값출력
+    console.log(t[key]);
+}
+
+console.log(t.name, t.age);
+
+with(t){
+    console.log(2, name, age);
+}
+*/
+
+/*
+// ex28)
+let str = '';
+str += 'let a = 10;'
+str += 'console.log(a);'
+console.log(str);
+eval(str); // 가급적 사용 X
+*/
+
+/*
+// ex27) 자습 - 비동기 처리(드림코딩)
+console.log('1');
+setTimeout(()=> console.log('2'), 1000);
+console.log('3');
+
+// Syschrobous callback
+function printImmediately(print){
+    print();
+}
+printImmediately(() => console.log('hello'));
+
+// Asyschrobous callback
+function printWithDelay(print, timeout){
+    setTimeout(print, timeout);
+}
+printWithDelay(() => console.log('async callback'),2000);
+*/
+/*
+// ex27)
+for(let i=0; i<3; i++){
+    (function(x){
+        setTimeout(
+            ()=>{ console.log(i); },
+            1000
+        )
+    })(i)
+}
+*/
+
+/*
+// ex26) 비동기 함수 예제
+console.log('1');
+
+// // setTimeout : 2초 뒤에 함수를 콜
+// setTimeout(
+//     function(){
+//         console.log('3');
+//     },
+//     2000 // 2초
+// )
+
+
+// 비동기 함수
+// setInterval : 2초에 한번씩 함수를 콜해라
+let num = 0;
+let id = setInterval(() => {
+        console.log(num++);
+},2000)
+
+ setTimeout(
+    () => {
+        clearInterval(id)
+        console.log('exit');
+    },
+    13000    
+ )
+
+console.log('2');
+*/
+
+/*
 // ex25)
 
 // 1
@@ -42,6 +372,7 @@ t = a => b => b + a;  // 인수 전달이 한 개일 때만 괄호 생략 가능
 
 // 7
 t = (a, c) => (b, d) => a*b+c*d;
+*/
 
 /*
 // ex24)
