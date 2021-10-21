78257a2a943fbe71cb30b274e17dc908b8c21149
ryeongeunkim <ryeongeun.gim@gmail.com>
2021-10-21 11:35:43 +0900

    21.10.21(목) redux, reduce 시작

--- Douzone/React/test02/App.js
+++ Douzone/React/test02/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
-  componentDidMount = async () => {
-    const response = await fetch('http://date.jsontest.com/', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: { a:"react", b:200 },
-    });
-    const body = await response.json();
-    alert(body.date)
-  }
-  
   render() {
     return (
-      <h1>fetch post</h1>
+      <div>
+        <h1>호랑이</h1>
+        <h1>{this.props.num}</h1>
+        <button onClick={this.props.onInc}>버튼</button>
+      </div>
     );
   }
 }
+// App : 이전값과 변경값이 동일하면 App로 안간다.
+const stateToProps = state => {
+  console.log('stateToProps', state.num);
+  return {num:state.num}; // num -> tttt도 가능
+}
 
+const dispatchToProps = dispatch => {
+  return {
+    //f1:()=>{}
+    // f1함수가 호출이 되면 'INC'문자열을 전송하시오
+    // 어디에? reducers
+    // f1:()=>{dispatch({type:'INC'});}
+    onInc:()=>{
+      console.log('dispatchToProps');
+      dispatch({type:'INC'});
+    }
+  };
+}
 
-export default App;
+// store ------>> num : 100
+export function reducers(state={num:100}, action){
+  console.log('reducers', action.type);
+  return { num:state.num + 1 } // num 변경하면 안됨
+}
+// store <<------ num : 101
+export default App = connect(
+  stateToProps,
+  dispatchToProps
+)(App);
--- Douzone/React/test02/index.js
+++ Douzone/React/test02/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// App 안에 reduce 함수를 들고와라
+import {reducers} from './App'; 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducers);
+
 ReactDOM.render(
-  //<React.StrictMode>
+  <Provider store={store}>
     <App />,
-  //</React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
