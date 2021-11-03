1f3b1b913668816139c9953485d2f3ac3327ca1b
ryeongeunkim <ryeongeun.gim@gmail.com>
2021-11-03 17:27:30 +0900

    test

--- Blog2/example-back/src/index.js
+++
@@ -1,6 +0,0 @@
-// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다
-/* eslint-disable no-global-assign */
-
-// import / export 문법 사용할 수 있도록 esm 모듈 사용.
-require = require('esm')(module /*, options*/);
-module.exports = require('./main.js');
